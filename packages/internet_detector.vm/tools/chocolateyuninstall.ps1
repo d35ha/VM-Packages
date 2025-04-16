0fdc8e652d10f3886fc32f95e63a6bdc6fde930a
d35ha <d35hax@gmail.com>
2025-04-16 18:45:04 +0800

    Start the internet detector at user logon immediately

--- packages/internet_detector.vm/tools/chocolateyuninstall.ps1
+++ packages/internet_detector.vm/tools/chocolateyuninstall.ps1
@@ -4,10 +4,19 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'internet_detector'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
+Disable-ScheduledTask -TaskName 'Internet Detector' -ea 0 | ForEach-Object {
+    $_ | Stop-ScheduledTask
+    $_ | Unregister-ScheduledTask -Confirm:$false
+}
+
+Get-Process -Name $toolName -ea 0 | ForEach-Object {
+    Stop-Process -Id $_.Id -Force
+    Wait-Process -Id $_.Id
+}
+
 VM-Uninstall $toolName $category
-Unregister-ScheduledTask -TaskName 'Internet Detector' -Confirm:$false
 
 $fakenetConfig = "$Env:RAW_TOOLS_DIR\fakenet\fakenet3.5\configs\default.ini"
 $config = Get-Content -Path $fakenetConfig
 $config = $config -replace '^.*BlackListIDsICMP.*$', "# BlackListIDsICMP: 1234"
-Set-Content -Path $fakenetConfig -Value $config -Encoding UTF8 -Force
+Set-Content -Path $fakenetConfig -Value $config -Encoding ASCII -Force
